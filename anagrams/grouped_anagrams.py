627c52d2ba4038790f6e4c044fbf16df0f2b3910
John Smith <thesacrade@gmail.com>
2025-09-02 17:14:12 +0300

    Add grouped_anagrams.py

---
+++ anagrams/grouped_anagrams.py
@@ -0,0 +1,19 @@
+#Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+from collections import defaultdict
+
+strs = ["ate", "eat", "tab", "tan", "nat", "bat"]
+class Solution:
+    def groupAnagrams(self, strs:list[str])-> list[list[str]]:
+        anagram_map = defaultdict(list)
+        
+        
+        for s in strs:
+            # Using tuple as a key since lists are not hashable
+            # sorting the string to create a unique key for anagrams
+            sorted_s = tuple(sorted(s))                      
+            anagram_map[sorted_s].append(s)
+            
+        return list(anagram_map.values())  
+    
+soln1 = Solution()
+print(soln1.groupAnagrams( ["ate", "eat", "tab", "tan", "nat", "bat"]))    
