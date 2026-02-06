38ca1ded30f2e4e1fd6d18df27129c3e5d538e06
John Smith <thesacrade@gmail.com>
2026-02-06 17:02:54 +0300

    Add password verification

--- python_encryption/password_protection.py
+++ python_encryption/password_protection.py
@@ -1,4 +1,6 @@
 from zxcvbn import zxcvbn
+from getpass import getpass 
+import bcrypt   
 
 def check_password_strength(password):
     result = zxcvbn(password)
@@ -10,11 +12,26 @@ def check_password_strength(password):
         if feedback['warning']:
             print("Warning:", feedback['warning'])
         if feedback['suggestions']:
-            print("Suggestions:", " ".join(feedback['suggestions']))
+            print("Suggestions:", ''.join(feedback['suggestions']))
     else:
         print("Password is strong.")
 
+def hash_password(password):
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(password.encode(), salt) 
+    return hashed
+
+def verify_password(password, hashed):
+    password = getpass("Enter your password again to verify: ")   
+    if bcrypt.checkpw(password.encode(), hashed):
+        return "Password is correct, access granted"
+    else:
+        return "Wrong Password, Access Denied." 
+
 if __name__ == "__main__":
-    password = input("Enter a pasword to check its strength: ")
+    password = getpass("Enter a password to check its strength: ")
     check_password_strength(password)
+    hashed_password = hash_password(password)
+    print(verify_password(password, hashed_password))
+
     
